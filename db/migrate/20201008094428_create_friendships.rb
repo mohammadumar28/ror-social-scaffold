38ad22b76f9fc8a55cce9ba8eec5a729f94ea559
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-10-09 05:40:23 -0300

    Add rspec tests and friendship methods

--- db/migrate/20201008094428_create_friendships.rb
+++ db/migrate/20201008094428_create_friendships.rb
@@ -1,8 +1,9 @@
 class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
-      t.references :user
-      t.references :friend, index: true, foreign_key: { to_table: :user }
+      t.integer :user_id, index: true, foreign_key: true
+      t.integer :friend_id, index: true, foreign_key: {to_table: :users}
+      t.boolean :confirmed
       t.timestamps
     end
   end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    @user = User.create(id: 1, name: 'aldana', email: 'aldu@gmail.com')
+    @user2 = User.create(id: 2, name: 'Umar', email: 'umar@gmail.com')
+    @friend = Friendship.create(user_id: 1, friend_id: 2, confirmed: true)
+  end 
+  
+  it 'returns confirmed friend' do
+    expect(@user.friends.first).to eq(@user2)
+  end
 end
