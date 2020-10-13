df8608f94c8e93d9cfed33701373d1b55f7cfc28
mohammadumar28 <mohammadumar28@gmail.com>
2020-10-13 14:49:32 +0530

    Add user_helper

--- app/helpers/friendship_helper.rb
+++ app/helpers/friendship_helper.rb
@@ -1,14 +1,13 @@
 module FriendshipHelper
 
-    def friend_inv(user)
-      return unless user.id != current_user.id
+  def friend_inv(user)
+    return unless user.id != current_user.id
 
-      return unless current_user.friends.include?(user)
+    return unless !current_user.friends.include?(user)
 
-      return unless !current_user.pending_friends.include?(user) 
+    return unless !current_user.pending_friends.include?(user)
 
-        link_to('Send frienship invitation', user_friendships_url(user_id: current_user.id, friend_id: user), method: :post)
-    end
-
+      link_to('Send frienship invitation', user_friendships_url(user_id: current_user.id, friend_id: user), method: :post)
+  end
 
 end
