a4f162073df641414e5d80dd09b63c645704162c
Muhammad Umar <mohammadumar28@gmail.com>
2020-10-15 20:59:44 +0530

    Revert "Milestone: Mutual Friendships"

--- app/helpers/friendship_helper.rb
+++ app/helpers/friendship_helper.rb
@@ -4,14 +4,9 @@ module FriendshipHelper
 
     return if current_user.friends.include?(user)
 
-    if !user.friend_requests.include?(current_user) && !current_user.friend_requests.include?(user)
+    return unless !current_user.pending_friends.include?(user) && !user.pending_friends.include?(current_user)
 
-      link_to('Send friendship invitation', user_friendships_url(user_id: current_user.id, friend_id: user),
-              method: :post, class: 'btn btn-sm btn-primary')
-    elsif user.friend_requests.include?(current_user)
-      content_tag(:div, 'Friend request already sent.', class: 'badge badge-info text-wrap')
-    else
-      content_tag(:div, 'Sent you an invitation', class: 'badge badge-warning text-wrap')
-    end
+    link_to('Send friendship invitation', user_friendships_url(user_id: current_user.id, friend_id: user),
+            method: :post, class: 'btn btn-primary')
   end
 end
