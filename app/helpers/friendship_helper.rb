06e8a4544a682c046987c4880ee1fdffd9194660
mohammadumar28 <mohammadumar28@gmail.com>
2020-10-15 20:56:04 +0530

    Add styles to the users page

--- app/helpers/friendship_helper.rb
+++ app/helpers/friendship_helper.rb
@@ -7,11 +7,11 @@ module FriendshipHelper
     if !user.friend_requests.include?(current_user) && !current_user.friend_requests.include?(user)
 
       link_to('Send friendship invitation', user_friendships_url(user_id: current_user.id, friend_id: user),
-              method: :post, class: 'btn btn-primary')
+              method: :post, class: 'btn btn-sm btn-primary')
     elsif user.friend_requests.include?(current_user)
-      content_tag(:p, 'Friend request already sent.')
+      content_tag(:div, 'Friend request already sent.', class: 'badge badge-info text-wrap')
     else
-      content_tag(:p, 'Sent you an invitation')
+      content_tag(:div, 'Sent you an invitation', class: 'badge badge-warning text-wrap')
     end
   end
 end
