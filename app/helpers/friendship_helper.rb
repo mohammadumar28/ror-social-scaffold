f89c61f41d7f8c0dae6035e7dcebeec5964a9459
mohammadumar28 <mohammadumar28@gmail.com>
2020-10-15 14:57:25 +0530

    Resolve errors in the RSpec tests

--- app/helpers/friendship_helper.rb
+++ app/helpers/friendship_helper.rb
@@ -4,9 +4,12 @@ module FriendshipHelper
 
     return if current_user.friends.include?(user)
 
-    return unless !current_user.friend_requests.include?(user) && !user.friend_requests.include?(current_user)
+    if !user.friend_requests.include?(current_user)
 
-    link_to('Send friendship invitation', user_friendships_url(user_id: current_user.id, friend_id: user),
-            method: :post, class: 'btn btn-primary')
+      link_to('Send friendship invitation', user_friendships_url(user_id: current_user.id, friend_id: user),
+              method: :post, class: 'btn btn-primary')
+    else
+      content_tag(:p, 'Friend request already sent.')
+    end
   end
 end
