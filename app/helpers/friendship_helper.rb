c16481bda691e84941214b229a82281b33b08ba1
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-10-15 09:21:49 -0300

    Fix logic error in friendship helper methods

--- app/helpers/friendship_helper.rb
+++ app/helpers/friendship_helper.rb
@@ -4,7 +4,7 @@ module FriendshipHelper
 
     return if current_user.friends.include?(user)
 
-    if !user.friend_requests.include?(current_user)
+    if !user.friend_requests.include?(current_user) && !current_user.friend_requests.include?(user)
 
       link_to('Send friendship invitation', user_friendships_url(user_id: current_user.id, friend_id: user),
               method: :post, class: 'btn btn-primary')
