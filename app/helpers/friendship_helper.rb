bd8043704d4b40a1f80d88d791846a0c445334d2
mohammadumar28 <mohammadumar28@gmail.com>
2020-10-16 14:44:30 +0530

    Change the title of the HTML page

--- app/helpers/friendship_helper.rb
+++ app/helpers/friendship_helper.rb
@@ -7,7 +7,7 @@ module FriendshipHelper
     if !user.friend_requests.include?(current_user) && !current_user.friend_requests.include?(user)
 
       link_to('Send friendship invitation', user_friendships_url(user_id: current_user.id, friend_id: user),
-              method: :post, class: 'btn btn-sm btn-primary')
+              method: :post, class: 'btn btn-sm btn-info')
     elsif user.friend_requests.include?(current_user)
       content_tag(:div, 'Friend request already sent.', class: 'badge badge-info text-wrap')
     else
